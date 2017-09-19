fa23b498417117b0d2aafda7b60b9472f25c67dc
Frederick Akalin <akalin@gmail.com>
2017-09-18 20:55:49 -0700

    Move into .h file

--- corefoundation.go
+++ corefoundation.go
@@ -5,65 +5,7 @@ package kext
 /*
 #cgo LDFLAGS: -framework CoreFoundation
 
-#include <stdint.h>
-#include <CoreFoundation/CoreFoundation.h>
-
-typedef uintptr_t CFTypeRefSafe;
-typedef uintptr_t CFStringRefSafe;
-typedef uintptr_t CFNumberRefSafe;
-typedef uintptr_t CFBooleanRefSafe;
-
-CFBooleanRefSafe kCFBooleanFalseSafe(void) {
-  return (CFBooleanRefSafe)kCFBooleanFalse;
-}
-
-CFBooleanRefSafe kCFBooleanTrueSafe(void) {
-  return (CFBooleanRefSafe)kCFBooleanTrue;
-}
-
-void CFReleaseSafe(CFTypeRefSafe cf) {
-  CFRelease((CFTypeRef)cf);
-}
-
-CFTypeID CFGetTypeIDSafe(CFTypeRefSafe cf) {
-  return CFGetTypeID((CFTypeRef)cf);
-}
-
-CFArrayRef CFArrayCreateSafe(CFAllocatorRef allocator, const uintptr_t *values, CFIndex numValues, const CFArrayCallBacks *callBacks) {
-  return CFArrayCreate(allocator, (const void **)values, numValues, callBacks);
-}
-
-void CFArrayGetValuesSafe(CFArrayRef theArray, CFRange range, const uintptr_t *values) {
-  return CFArrayGetValues(theArray, range, (const void **)values);
-}
-
-CFDictionaryRef CFDictionaryCreateSafe(CFAllocatorRef allocator, const uintptr_t *keys, const uintptr_t *values, CFIndex numValues, const CFDictionaryKeyCallBacks *keyCallBacks, const CFDictionaryValueCallBacks *valueCallBacks) {
-  return CFDictionaryCreate(allocator, (const void **)keys, (const void **)values, numValues, keyCallBacks, valueCallBacks);
-}
-
-const char * CFStringGetCStringPtrSafe(CFStringRefSafe theString, CFStringEncoding encoding) {
-  return CFStringGetCStringPtr((CFStringRef)theString, encoding);
-}
-
-CFIndex CFStringGetLengthSafe(CFStringRefSafe theString) {
-  return CFStringGetLength((CFStringRef)theString);
-}
-
-CFIndex CFStringGetBytesSafe(CFStringRefSafe theString, CFRange range, CFStringEncoding encoding, UInt8 lossByte, Boolean isExternalRepresentation, UInt8 *buffer, CFIndex maxBufLen, CFIndex *usedBufLen) {
-  return CFStringGetBytes((CFStringRef)theString, range, encoding, lossByte, isExternalRepresentation, buffer, maxBufLen, usedBufLen);
-}
-
-CFNumberType CFNumberGetTypeSafe(CFNumberRefSafe number) {
-  return CFNumberGetType((CFNumberRef)number);
-}
-
-Boolean CFNumberGetValueSafe(CFNumberRefSafe number, CFNumberType theType, void *valuePtr) {
-  return CFNumberGetValue((CFNumberRef)number, theType, valuePtr);
-}
-
-Boolean CFBooleanGetValueSafe(CFBooleanRefSafe boolean) {
-  return CFBooleanGetValue((CFBooleanRef)boolean);
-}
+#include "CoreFoundationSafe.h"
 */
 import "C"
 import (
